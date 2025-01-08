1bf40741431ae180ac8187e89e54d814af2dae8a
bianxcafaria <biancahfaria@hotmail.com>
2025-01-08 16:59:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "UHU!!!!!!!";
+  gif.src =
+    "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExZ2U2ejIweG9yNzQ4b2hsN3R5ZTludjRzYzZpZjJ4bmRjY3Jkc2Z0ayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3ohzdRwS3UXGtsBVBK/giphy.webp";
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
